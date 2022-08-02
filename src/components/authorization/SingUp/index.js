344d4f8cd6f2eeae870d441f8166d6ef79458f85
Bartlomiej Wilczynski <72442528+BWilczynski98@users.noreply.github.com>
2022-08-02 16:07:08 +0200

    A new global color has been added. Components have been created to user login/sing in/reset password

---
+++ src/components/authorization/SingUp/index.js
@@ -0,0 +1,2 @@
+import SingUp from "./SingUp";
+export { SingUp };
---
+++ src/components/authorization/Styles/Error.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Alert } from "@mui/material";
+
+export const StyledAlert = styled(Alert)`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+`;
