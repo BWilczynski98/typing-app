206bd4bf31e743e81f98577a2dd07e978d773f64
Bartlomiej Wilczynski <72442528+BWilczynski98@users.noreply.github.com>
2022-08-05 16:03:49 +0200

    PTT-7 Logic that handles events such as. Compare whether the rewritten word matches the word in the test field. In Create a pattern for transcription. In real time, the word is highligted in red if there is an error. Word validation when the user clicks a space. The table on which the transcribed text is displayed completes after each finished line of text. The result is determined in words per minute, charts per minute and accurace

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { AccountProfile } from "./components/AccountProfile";
+
 
 const App = () => {
-  return (
-    <>
-      <AccountProfile />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
